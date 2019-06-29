0488e95e0050a94120e192e6a72d3da6293e492c
yanzhi_bai <yanzhi.bai@airbnb.com>
2019-06-28 22:38:18 -0700

    spr from yahoo, in progress

---
+++ read_symbols.py
@@ -0,0 +1,42 @@
+import MySQLdb
+from yahoo_finance import parse_psr_from_yahoo
+import time
+
+
+def chunks(l, n):
+    # looping till length l
+    for i in range(0, len(l), n):
+        yield l[i:i + n]
+
+
+def get_all_prs():
+    # Open database connection
+    db = MySQLdb.connect("localhost", "root", "", "finance")
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    sql = "SELECT * FROM raw_symbols "
+
+    cursor.execute(sql)
+    results = cursor.fetchall()
+
+    for row_batch in chunks(results, 10):
+        for row in row_batch:
+            try:
+                symbol = row[0]
+                print('Data: {}: {}'.format(symbol, parse_psr_from_yahoo(symbol)))
+
+            except Exception as e:
+                print("Error: ", e, row[0])
+
+        print('sleeping 5 sec')
+        time.sleep(5)
+
+    # disconnect from server
+    db.close()
+
+
+if __name__ == "__main__":
+    print("Fetching all data for prs from yahoo")
+    get_all_prs()
---
+++ yahoo_finance.py
@@ -0,0 +1,37 @@
+import argparse
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def parse_psr_from_yahoo(ticker):
+	url = "http://finance.yahoo.com/quote/{}/key-statistics?p={}&.tsrc=fin-srch".format(ticker,ticker)
+
+	try:
+		web_data = requests.get(url)
+		beautify_data = BeautifulSoup(web_data.content, "html.parser")
+
+		rows = beautify_data.find_all('tr')
+		for row in rows:
+			cols = row.find_all('td')
+			if len(cols) < 2: continue
+			if cols[0].text.startswith('Price/Sales'):
+				return float(cols[1].text)
+		return 0
+	except Exception as e:
+		raise Exception("Failed to get yahoo psr data", e)
+
+
+if __name__=="__main__":
+	argparser = argparse.ArgumentParser()
+	argparser.add_argument('ticker',help = '')
+	args = argparser.parse_args()
+	ticker = args.ticker
+
+	print ("Fetching data for %s"%(ticker))
+	scraped_data = parse_psr_from_yahoo(ticker)
+	print(scraped_data)
+
+	# print ("Writing data to output file")
+	# with open('%s-summary.json'%(ticker),'w') as fp:
+	# 	json.dump(scraped_data,fp,indent = 4)
